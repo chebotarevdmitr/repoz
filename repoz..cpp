2329f5a1452ef7117c9bb5147ee110aed1f41b54
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2024-02-21 21:10:11 +0300

    add second and third string

--- repoz..cpp
+++ repoz..cpp
@@ -1,9 +1,10 @@
+
 #include <iostream>  
 
 int main(int argc, char** argv) {
-  std::cout<<"hello Dimchasic" <<std::endl;
-   
-   	
+    std::cout<<"hello Dimchasic" <<std::endl;
+    std::cout<<"add second line" <<std::endl;
+    std::cout<<"add thri line"   <<std::endl;	
 system("pause") ;	
 	return 0;
 }
